a878b4511ef7c874f63694428feaad6fff55bdb8
cruzerngz <cruzerngz+github@gmail.com>
2021-11-06 18:59:03 +0800

    syncing with PC

--- src/debugging.java
+++ src/debugging.java
@@ -1,11 +1,14 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Arrays;
+
 import util.*;
 
 public class debugging {
     public static void main(String[] args) throws IOException {
-        String path = "./util/2022.csv";
+        String path = "./util/testdata.csv";
 
         ArrayList<String[]> data = Data.readCSV(path);
         for(int i=0; i<data.size(); i++) {
@@ -15,6 +18,26 @@ public class debugging {
             System.out.println();
         }
 
-        Data.writeCSV(data, "bla2022.csv");
+        //Data.writeCSV(data, "bla.csv");
+        HashMap<String, String[]> result;
+        result = Data.parse(data);
+
+        String key;
+
+        //System.out.println(Arrays.asList(result));
+        key = "header2";
+        //System.out.println(key + Arrays.toString(result.get(key)));
+
+        // HashMap<String,String> thisthing = new HashMap<String, String>();
+        // thisthing.put("1","10");
+        // thisthing.put("2","20");
+        // thisthing.put("3","30");
+
+        // for(String thing: thisthing.keySet()) {
+        //     key = thing.toString();
+        //     System.out.print(key);
+        //     System.out.println(thisthing.get(key));
+        // }
+        
     }
 }
--- src/util/Data.java
+++ src/util/Data.java
@@ -6,17 +6,26 @@ import java.io.FileWriter;
 import java.io.BufferedWriter;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 //exceptions
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
 ///import Json
 /**
- * Collection of data parsing functions  
+ * Collection of data parsing functions 
  * For reading and writing to file
+ * @author cruzerngz
  */
 public class Data {
 
+    /**
+     * Reads csv to an arrayList of String arrays
+     * @param filePath Relative path to file
+     * @return ArrayList containing CSV data
+     * @throws FileNotFoundException
+     */
     public static ArrayList<String[]> readCSV(String filePath) throws FileNotFoundException {
         //read the first line of the csv file
         //write each line as an array to the return arraylists
@@ -32,8 +41,13 @@ public class Data {
         return returnArr;
     }
 
-
-    public static void writeCSV(ArrayList<String[]> writeArr, String path) throws IOException {
+    /**
+     * Writes arrayList to a CSV file
+     * @param writeArr Data to be written
+     * @param filePath Relative path to file
+     * @throws IOException
+     */
+    public static void writeCSV(ArrayList<String[]> writeArr, String filePath) throws IOException {
         String writeStr = "";
 
         for(int i=0; i<writeArr.size(); i++) {
@@ -41,8 +55,49 @@ public class Data {
             writeStr += "\n";
         }
 
-        BufferedWriter writer = new BufferedWriter(new FileWriter(path));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(filePath));
         writer.write(writeStr);
         writer.close();
+    }
+
+    /**
+     * Convert an ArrayList into a map.
+     * The first array (interpreted as column headers) is converted into HashMap keys.
+     * The data entries are String arrays behind each header (key).
+     */
+    public static HashMap<String, String[]> parse(ArrayList<String[]> arrIn) {
+
+        int rows = arrIn.size();        //number of rows
+        int cols = arrIn.get(0).length; //number of cols
+        HashMap<String, String[]> returnMap = new HashMap<String, String[]>();
+        String[] tempData = new String[rows-1];
+        String tempKey;
+
+        //build the HashMap
+        for(int i=0; i<cols; i++) {
+            Arrays.fill(tempData, ""); //reset elements in array
+
+            //build column data
+            for(int j=0; j<rows-1; j++) {
+                tempData[j] = arrIn.get(j+1)[i];
+            }
+            tempKey = arrIn.get(0)[i];
+            System.out.println(tempKey + Arrays.toString(tempData));
+
+            //add to map
+            returnMap.put(arrIn.get(0)[i], tempData);
+            for(String thing: returnMap.keySet()) {
+                System.out.println(thing + " " + Arrays.toString(returnMap.get(thing)));
+            }   
+        }
+        return returnMap;
+    }
+
+    public static ArrayList<String[]> parse(HashMap<String, String[]> mapIn) {
+
+        ArrayList<String[]> returnArr = new ArrayList<String[]>();
+
+
+        return returnArr;
     }
 }
