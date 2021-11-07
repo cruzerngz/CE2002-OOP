b2278bb092bc5ce4d65b1b8ecbfaea3f2edfa191
DomdomUnilife <93030182+DomdomUnilife@users.noreply.github.com>
2021-11-07 23:43:31 +0800

    Domi tried, and failed

--- src/objects/Order.java
+++ src/objects/Order.java
@@ -1,5 +1,9 @@
 package objects;
+import java.util.ArrayList;
 import java.util.Scanner;
+
+import util.Data;
+import util.DateTime;
 
 public class Order {
     //construct staff object
@@ -10,23 +14,75 @@ public class Order {
         //staffid could be part of the constructor
     }
 
-    public void create() {
+    public static int create() {
         //creates a new order
-
-        //can edit, can delete
-        //leads to checkout
+        ArrayList<String[]> temp = Data.readCSV("./Order.txt"); //how to put filepath?
+        int i=0;
+        while( i<temp.size() )
+        {
+            if(temp[i][1] == 0) //index 1 is orderid field and is null
+                break; //get out of loop with i pointing to which row we want to edit
+            else ++i;
+        }
+        String id = DateTime.getDateTime();
+        temp[i][1] = id; //write to array
+        Data.writeCSV(temp, Order.csv);
+        return id;
     }
 
-    public void printOrder(int orderID) {
+    public static void printOrder(Integer orderID) {
+        ArrayList<String[]> temp = Data.readCSV("./Order.csv"); //read into arraylist
+        int i=0;
+        while(temp[i][1] != orderID.toString() && i<temp.size() ) //no match and never reach end
+            ++i;
+        if( i==temp.size() )
+        {
+            System.out.println("No such order found");
+            return;
+        }
+        // by here i is the row we want to print
+        System.out.println(temp[i]); //print string array
 
     }
 
-    public void addItem(int orderID) {
+    public static void addItem(int orderID) {
+        ArrayList<String[]> temp = Data.readCSV(./Order.csv);
+        int i=0;
+        while(temp[i][1] != orderID.toString() && i<temp.size() ) //no match and never reach end
+            ++i;
+        if( i==temp.size() )
+        {
+            System.out.println("No such order found");
+            return;
+        }
+        System.out.println("What item are you adding?");
+        Scanner sc = new Scanner(System.in);
+        String itemID = sc.next();
+        sc.close();
+        //add item to arraylist and WB
+        temp[i][4] ;//items column how to append?
 
+        Data.writeCSV(temp, "Order.csv");
     }
 
-    public void removeItem(int orderID) {
-        
+    public static void removeItem(int orderID) {
+        ArrayList<String[]> temp = Data.readCSV(./Order.csv);
+        int i=0;
+        while(temp[i][1] != orderID.toString() && i<temp.size() ) //no match and never reach end
+            ++i;
+        if( i==temp.size() )
+        {
+            System.out.println("No such order found");
+            return;
+        }
+        System.out.println("What item are you removing?");
+        Scanner sc = new Scanner(System.in);
+        String itemID = sc.nextLine();
+        //look for item in that index and remove
+        //how to access the particular string and search?
+
+        //WB to csv
+        Data.writeCSV(temp, "./Order.csv");
     }
 
 }
