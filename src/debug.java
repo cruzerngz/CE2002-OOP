11b4914f4f82a581ee9df32851963040f2a2f6cc
cruzerngz <61926974+cruzerngz@users.noreply.github.com>
2021-11-08 23:37:40 +0800

    added persistence to vars
    multiple calling class will get the same offsets

--- src/debug.java
+++ src/debug.java
@@ -1,37 +1,36 @@
+import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 
 import util.*;
 
 public class debug {
-    public static void main(String args[]) throws NoSuchAlgorithmException {
-        DateTime datetime = new DateTime();
+    public static void main(String args[]) throws IOException {
+        // DateTime datetime = new DateTime();
 
+        // System.out.println(datetime.getDaysSinceEpoch());
+        // System.out.println(datetime.daysToDate(18900));
+        // System.out.println(datetime.getTime());
+        // System.out.println(datetime.getTimeString());
+
+        // String z = "\u001B[0m";
+        // String x = "this is coloured";
+
+        // x = StrColour.Green(x);
+        // System.out.println(x);
+
+        // //my precious regex
+        // //filters out the unicode special characters
+        // String regex = "\\e\\[[0-9]+m";
+        // x = x.replaceAll(regex, "");
+        // System.out.println(x);
+        // System.out.println(Password.hash("inpuu"));
+        testTime();
+    }
+    public static void testTime() throws IOException {
+        DateTime datetime = new DateTime("../data");
         System.out.println(datetime.getTime());
-        System.out.println(StrColour.Red(datetime.getDate()));
-        System.out.println(datetime.getDateTime());
-
-        datetime.deltaDay(-100);
-        datetime.deltaHour(-1203);
-        System.out.println(datetime.getTime());
-        System.out.println(datetime.getDate());
-        System.out.println(datetime.getDateTime());
-
+        datetime.deltaHour(1);
+        datetime.deltaDay(0);
         datetime.reset();
-        System.out.println(datetime.getTime());
-        System.out.println(datetime.getDate());
-        System.out.println(datetime.getDateTime());
-
-        String z = "\u001B[0m";
-        String x = "this is coloured";
-
-        x = StrColour.Green(x);
-        System.out.println(x);
-
-        //my precious regex
-        //filters out the unicode special characters
-        String regex = "\\e\\[[0-9]+m";
-        x = x.replaceAll(regex, "");
-        System.out.println(x);
-        System.out.println(Password.hash("inpuu"));
     }
 }
--- src/util/DateTime.java
+++ src/util/DateTime.java
@@ -1,6 +1,9 @@
 package util;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 //imports
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 /**
  * Date-Time object that keeps track of the current date and time,
@@ -8,9 +11,10 @@ import java.text.SimpleDateFormat;
  * @author cruzerngz
  */
 public class DateTime {
-    private long deltaH;
-    private long deltaD;
+    private long deltaH = 0;
+    private long deltaD = 0;
     private long unixTime;
+    private String savePath;
 
     //formatters
     static SimpleDateFormat timeFormat = new SimpleDateFormat("HHmm");
@@ -18,9 +22,13 @@ public class DateTime {
     static SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
     static SimpleDateFormat dateTimeFormat = new SimpleDateFormat("E dd/MM/yyyy HH:mm");
 
-    public DateTime() {
-        deltaH = 0; //hour offset
-        deltaD = 0; //day offset
+    /**
+     * Constructor
+     * @param filePath Path to data directory
+     * @throws FileNotFoundException
+     */
+    public DateTime(String filePath) throws FileNotFoundException {
+        savePath = filePath + "/timeData.csv";
         update();
     }
 
@@ -28,7 +36,7 @@ public class DateTime {
      * Gets the number of days passed since 1 Jan 1970
      * @return Int days elapsed since epoch
      */
-    public int getDaysSinceEpoch() {
+    public int getDaysSinceEpoch() throws IOException {
         update();
         return (int)(unixTime / 1000 / 86400);
     }
@@ -44,21 +52,11 @@ public class DateTime {
     }
 
     /**
-     * **UNUSED**  
-     * Converts the number of days since 1970 into date
-     * @param daysSinceEpoch Days elapsed since epoch
-     * @return Date-time in string form 
-     */
-    private String daysToDateTime(int daysSinceEpoch) {
-        long time = (long)(daysSinceEpoch) * 86400 * 1000;
-        return dateTimeFormat.format(time);
-    }
-
-    /**
      * Gets the current time with offsets
      * @return 24H format time
+     * @throws IOException
      */
-    public int getTime() {
+    public int getTime() throws IOException {
         update();
         return Integer.parseInt(timeFormat.format(unixTime));
     }
@@ -66,8 +64,9 @@ public class DateTime {
     /**
      * Gets the current time with offsets
      * @return 24H format time as string
+     * @throws IOException
      */
-    public String getTimeString() {
+    public String getTimeString() throws IOException {
         update();
         return timeStringFormat.format(unixTime);
     }
@@ -76,7 +75,7 @@ public class DateTime {
      * Gets the current date with offsets
      * @return Date in string form DD/MM/YYYY
      */
-    public String getDate() {
+    public String getDate() throws IOException {
         update();
         return dateFormat.format(unixTime);
     }
@@ -85,7 +84,7 @@ public class DateTime {
      * Gets the current date and time with offsets
      * @return Date-time in string form
      */
-    public String getDateTime() {
+    public String getDateTime() throws IOException {
         update();
         return dateTimeFormat.format(unixTime);
     }
@@ -94,32 +93,58 @@ public class DateTime {
      * Add or subtract hours to the current time
      * @param delta Hour delta
      */
-    public void deltaHour(long delta) {
+    public void deltaHour(long delta) throws IOException {
         deltaH += delta * 3600 * 1000;
+        save();
     }
 
     /**
      * Add or subtract days to the current time
      * @param delta Day delta
      */
-    public void deltaDay(long delta) {
+    public void deltaDay(long delta) throws IOException {
         deltaD += delta * 86400 * 1000;
+        save();
     }
 
     /**
      * Remove all hour and day offsets
+     * @throws IOException
      */
-    public void reset() {
+    public void reset() throws IOException {
         deltaH = 0;
         deltaD = 0;
+        save();
     }
 
     /**
      * Updates the current private variable time
+     * Reads saved modifiers from file
+     * @throws FileNotFoundException
      */
-    private void update() {
+    private void update() throws FileNotFoundException {
         unixTime = System.currentTimeMillis();
+
+        ArrayList<String[]> dataArr = Data.readCSV(savePath);
+        deltaH = Long.parseLong(dataArr.get(0)[0]); //hour offset
+        deltaD = Long.parseLong(dataArr.get(0)[1]); //day offset
+        
         unixTime += deltaH;
         unixTime += deltaD;
+    }
+
+    /**
+     * Saves modifiers to file
+     * @throws IOException
+     */
+    private void save() throws IOException {
+        String[] writeArr = new String[2];
+        ArrayList<String[]> toWrite = new ArrayList<String[]>();
+
+        writeArr[0] = Long.toString(deltaH);
+        writeArr[1] = Long.toString(deltaD);
+        toWrite.add(writeArr);
+
+        Data.writeCSV(toWrite, savePath);
     }
 }
