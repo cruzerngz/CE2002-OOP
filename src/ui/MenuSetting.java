478214e394a1348b575dca2c41eb0a1d44156d99
DomdomUnilife <93030182+DomdomUnilife@users.noreply.github.com>
2021-11-14 21:01:16 +0800

    small fixes to ordering

--- src/ui/MenuSetting.java
+++ src/ui/MenuSetting.java
@@ -30,7 +30,7 @@ public class MenuSetting implements BaseUI{
             Colour.println(Colour.TEXT_GREEN,"(0) Back");
 
             System.out.println("");
-            System.out.printf("Enter your choice: ");
+            Colour.println(Colour.TEXT_GREEN, "Enter your choice: ");
             
             choice = sc.nextInt();
             String filepath = "menu.csv";
@@ -89,10 +89,12 @@ public class MenuSetting implements BaseUI{
                 
                 this.deleteItem(filepath, Iddelete);
                 break;
-            case 5:
+            case 0:
                 System.out.println("Going back ….");
                 break;
             default:
+                System.out.println("Invalid choice");
+                break;
             }
         } while (choice != 0);
     }
