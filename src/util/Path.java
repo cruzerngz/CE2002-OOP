e0e5e49704262bf9ce5c30b11ab7213c99b8791a
weejunyap <weejunyap@gmail.com>
2021-11-15 01:06:06 +0800

    Promo menu changes

--- src/util/Path.java
+++ src/util/Path.java
@@ -6,7 +6,7 @@ package util;
 public class Path {
     public static final String menu = "../data/menu.csv";
     public static final String order = "../data/Order.csv";
-    public static final String pMenu = "../data/pmenu.csv";
+    public static final String pMenu = "../data/promomenu.csv";
     public static final String reservations = "../data/reservations.csv";
     public static final String revMatrix = "../data/revenueMatrix.csv";
     public static final String staff = "../data/Staffroster.csv";
