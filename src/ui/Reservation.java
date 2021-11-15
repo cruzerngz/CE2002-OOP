77fbd7db5630a8a93eab799dd275cd3db4ad53cb
weejunyap <weejunyap@gmail.com>
2021-11-15 17:32:55 +0800

    adding of time expiration for check reserve

--- src/ui/Reservation.java
+++ src/ui/Reservation.java
@@ -82,6 +82,7 @@ public class Reservation implements BaseUI{
                     switch (choice) {
                     case 1:
                         System.out.println("Enter date:");
+                        sc.nextLine();
                         String date4 = sc.nextLine();
                         res.showReservationStatus_Day(date4);
                         break;
